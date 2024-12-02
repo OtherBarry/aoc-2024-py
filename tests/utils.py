2536c1f76fc6527dcf8391e602d2d2fbd90eaa7a
Charlie Wilson <charlie.wilson@q-ctrl.com>
2024-12-02 22:39:09 +1100

    feat: Setup testing

---
+++ tests/utils.py
@@ -0,0 +1,31 @@
+from importlib import import_module
+from pathlib import Path
+from typing import TYPE_CHECKING, Any
+
+if TYPE_CHECKING:
+    from solutions.base import BaseSolution
+
+
+def assert_solution_part_returns_expected(
+    problem: int, part: int, expected: int
+) -> None:
+    """
+    Assert that the solution part returns the expected value
+
+    :param solution_id: The solution ID
+    :param part: The part of the solution
+    :param expected: The expected value
+    """
+    input_path = Path(f"inputs/{problem:02d}.txt")
+    solution_class: type[BaseSolution[Any, Any]] = import_module(
+        f"solutions.day{problem:02d}"
+    ).Solution
+    solution = solution_class(input_path)
+    solution.setup()
+    if part == 1:
+        assert solution.part_1() == expected  # noqa: S101
+    elif part == 2:
+        assert solution.part_2() == expected  # noqa: S101
+    else:
+        msg = f"Invalid part: {part}"
+        raise ValueError(msg)
