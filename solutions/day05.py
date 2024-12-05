033b4f5b29d3bb31243d0adf592595f2a3379ae3
Charlie Wilson <charlie.wilson@q-ctrl.com>
2024-12-06 09:22:17 +1100

    refactor: Improve day 5 naming

--- solutions/day05.py
+++ solutions/day05.py
@@ -2,26 +2,26 @@ from solutions.base import BaseSolution
 
 
 class Page:
-    def __init__(self, value: int) -> None:
-        self.value = value
+    def __init__(self, page_number: int) -> None:
+        self.page_number = page_number
         self._must_come_before = set[int]()
 
-    def add_rule(self, must_come_before: int) -> None:
-        self._must_come_before.add(must_come_before)
+    def must_come_before(self, page_number: int) -> None:
+        self._must_come_before.add(page_number)
 
     def __lt__(self, other: object) -> bool:
         if not isinstance(other, Page):
             return NotImplemented
-        return other.value in self._must_come_before
+        return other.page_number in self._must_come_before
 
     def __eq__(self, other: object) -> bool:
         if not isinstance(other, Page):
             return NotImplemented
-        return self.value == other.value
+        return self.page_number == other.page_number
 
 
-def middle_value(values: list[Page]) -> int:
-    return values[len(values) // 2].value
+def get_middle_page_number(pages: list[Page]) -> int:
+    return pages[len(pages) // 2].page_number
 
 
 class Solution(BaseSolution):
@@ -31,7 +31,7 @@ class Solution(BaseSolution):
         pages = dict[int, Page]()
         for rule_line in raw_rules.splitlines():
             page, before = rule_line.split("|")
-            pages.setdefault(int(page), Page(int(page))).add_rule(int(before))
+            pages.setdefault(int(page), Page(int(page))).must_come_before(int(before))
         self.updates = [
             [pages[int(page)] for page in update.split(",")]
             for update in raw_updates.splitlines()
@@ -44,9 +44,9 @@ class Solution(BaseSolution):
         for update in self.updates:
             sorted_update = sorted(update)
             if sorted_update == update:
-                self._part_1_result += middle_value(update)
+                self._part_1_result += get_middle_page_number(update)
             else:
-                self._part_2_result += middle_value(sorted_update)
+                self._part_2_result += get_middle_page_number(sorted_update)
 
     def part_1(self) -> int:
         return self._part_1_result
